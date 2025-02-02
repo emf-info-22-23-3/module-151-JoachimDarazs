543dc2697bb8a6bbd4ae35c72c00e784dc9396f1
JoachimDarazs <nastudragnir4@gmail.com>
2025-02-02 22:11:24 +0100

    exercice8fun

---
+++ src/exercices/exercice8/serveur/ctrl/Ctrl.php
@@ -0,0 +1,44 @@
+<?php
+
+class Ctrl {
+    
+    private $wrk;
+
+    public function __construct() {
+        $this->wrk = new Wrk();
+    }
+
+    public function getJoueursXML($idEquipe)
+    {
+        $joueurs = $this->wrk->getJoueurs($idEquipe);
+        
+        $xml = new SimpleXMLElement('<joueurs></joueurs>');
+        
+        foreach ($joueurs as $joueur) {
+            $joueurElement = $xml->addChild('joueur');
+            $joueurElement->addChild('id', $joueur->getId());
+            $joueurElement->addChild('nom', $joueur->getName());
+            $joueurElement->addChild('equipe', $joueur->getFKEquipe());
+            $joueurElement->addChild('points', $joueur->getPoints());
+        }
+
+        return $xml->asXML();
+    }
+
+    public function getEquipesXML()
+    {
+        $equipes = $this->wrk->getEquipes();
+        
+        $xml = new SimpleXMLElement('<equipes></equipes>');
+        
+        foreach ($equipes as $equipe) {
+            $equipeElement = $xml->addChild('equipe');
+            $equipeElement->addChild('id', $equipe->getId());
+            $equipeElement->addChild('nom', $equipe->getNom());
+        }
+
+        return $xml->asXML();
+    }
+
+
+}
--- src/exercices/exercice8/serveur/serveur.php
+++ src/exercices/exercice8/serveur/serveur.php
@@ -1,7 +1,19 @@
 <?php
-require_once 'Wrk.php';
-require_once 'Equipe.php';
+require_once './wrk/Wrk.php';
+require_once './beans/Equipe.php';
+require_once './beans/Joueur.php';
+require_once './ctrl/Ctrl.php';
 
-$wrk = new Wrk();
-$equipes = $wrk->getEquipes();
-echo json_encode($equipes);
+$ctrl = new Ctrl();
+
+if($_GET['action'] == "equipe")
+{
+	$equipes = $ctrl->getEquipesXML();
+    echo $equipes;
+}
+
+if($_GET['action'] == "joueur")
+{
+    $joueurs = $ctrl->getJoueursXML($_GET['equipeId']);
+    echo $joueurs;
+}
