37366385c01126cec391d50c0ad11155ecbc5d6d
JoachimDarazs <nastudragnir4@gmail.com>
2025-02-03 14:08:41 +0100

    ex9

--- src/exercices/exercice9/serveur/serveur.php
+++ src/exercices/exercice9/serveur/serveur.php
@@ -1,28 +1,47 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-	if($_POST['action'] == "connect")
-	{
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	if ($_POST['action'] == "connect") {
+
 		// doit controller que le password donné (en post) est bien 'emf'
 		// si c'est ok => enregistrement de "emf" dans une variable de session et écrire '<result>true</result>'
 		// si c'est faux => effacer la variable de session et écrire '<result>false</result>'
-		echo '<result>false</result>';
+		if ($_POST['password'] == "emf") {
+			$_SESSION['logged'] = $_POST['password'];
+			echo '<result>true</result>';
+
+
+		} else {
+			session_unset();
+			echo '<result>false</result>';
+		}
+
 	}
 
-	if($_POST['action'] == "disconnect")
-	{
+	if ($_POST['action'] == "disconnect") {
 		// effacer la variable de session et écrire <result>true</result>
+		session_unset();
+		
 		echo '<result>true</result>';
 	}
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET'){
-	if($_GET['action'] == "getInfos")
-	{
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+	if ($_GET['action'] == "getInfos") {
 		// tout d'abord contrôler que la variable de session 'logged' contient bien "emf"
 		// si oui => retourner <users><user><name>Victor Legros</name><salaire>9876</salaire></user><user><name>Marinette Lachance</name><salaire>7540</salaire></user><user><name>Gustave Latuile</name><salaire>4369</salaire></user><user><name>Basile Ledisciple</name><salaire>2384</salaire></user></users>
 		// si non => écrire <message>DROITS INSUFFISANTS</message>
-		echo '<message>DROITS INSUFFISANTS</message>';
+		if (isset($_SESSION['logged']) && $_SESSION['logged'] == "emf") {
+			echo '<users><user><name>Victor Legros</name><salaire>9876</salaire></user><user><name>Marinette Lachance</name><salaire>7540</salaire></user><user><name>Gustave Latuile</name><salaire>4369</salaire></user><user><name>Basile Ledisciple</name><salaire>2384</salaire></user></users>';
+
+		} else {
+			echo '<message>DROITS INSUFFISANTS</message>';
+
+		}
+
+
 	}
 }
 
